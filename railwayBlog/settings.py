ef600c15856b05e53f1424025d22fb7611bcc849
alfani <alfanishekw@gmail.com>
2023-05-11 11:41:53 +0300

    Modifef2

--- railwayBlog/settings.py
+++ railwayBlog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6da-vp)^2&sp(u%h=gk@17fsgzqj63mp(qs%%e%lqcv41$8vhd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
