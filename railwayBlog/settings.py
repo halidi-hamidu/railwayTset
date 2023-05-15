728a22c220cdd14420adef930a5a781277f5fa94
alfani <alfanishekw@gmail.com>
2023-05-15 09:59:27 +0300

    Add railway models

--- railwayBlog/settings.py
+++ railwayBlog/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+DATABASE_URL = "PGPASSWORD=rVsTX0IjiNkxOONSMQJ8 psql -h containers-us-west-162.railway.app -U postgres -p 5782 -d railway"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -76,10 +77,7 @@ WSGI_APPLICATION = 'railwayBlog.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
 }
 
 
